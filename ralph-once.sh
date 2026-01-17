565bac46724d6eaf7e56ae1f7bc149951d5a7b2b
Michael Holley <michael.philipp.holley@gmail.com>
2026-01-17 15:43:18 +0100

    add ralph-once

---
+++ ralph-once.sh
@@ -0,0 +1,7 @@
+opencode run "@requirements.md @progress.md  \
+1. Read the requirements.md and progress.md files. \
+2. Find the next incomplete task and implement it. \
+3. Update progress.md with what you did. \
+4. Ensure the codebase is stable by successfully running all automated tests, static type-checks, and the production build process according to the project's specific toolchain. \
+5. Commit your changes. \
+ONLY DO ONE TASK AT A TIME." -m "github-copilot/claude-sonnet-4.5"
